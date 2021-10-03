85d46b81a366baff3ffe612626808f371f966980
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-03 18:04:19 -0500

    created dispatch in moviedetails component to fetch movie details

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -6,12 +6,17 @@ function MovieDetails() {
     const dispatch = useDispatch();
     const history = useHistory();
 
+    useEffect( () => {
+        dispatch({ type: 'FETCH_MOVIE_DETAILS' })
+    }, [])
+
     const backToMovieList = () => {
         history.push('/')
     }
     return(
         <div>
             <h2>Movie Details for ???</h2>
+
             <button onClick={backToMovieList}>Back To Movie List</button>
         </div>
 

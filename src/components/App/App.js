b658a8f7faa92cb4b9e5dfdb7726030a9f1a2d81
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-03 17:43:07 -0500

    created a click funtion to allow for navigation to movie details via image click

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,21 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import MovieDetails from '../MovieDetails/MovieDetails';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
+
         <Route path="/" exact>
           <MovieList />
         </Route>
         
-        {/* Details page */}
+        <Route path="/details" exact>
+          <MovieDetails />
+        </Route>
 
         {/* Add Movie page */}
       </Router>

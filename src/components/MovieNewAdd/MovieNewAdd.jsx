79dcbd55df861454a06228b2543e0b200643ca52
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-03 22:25:14 -0500

    attempting new approach at input for new movie

--- src/components/MovieNewAdd/MovieNewAdd.jsx
+++ src/components/MovieNewAdd/MovieNewAdd.jsx
@@ -1,37 +1,73 @@
 import { useDispatch } from "react-redux";
 
 function MovieNewAdd() {
-    const [ title, submitTitle ] = useState('')
-    const [ description, submitDescription ] = useState('')
-    const [ poster, submitPoster] = useState('')
-    const [ genre_id, submitGenre_id] = useState('')
+    const [ newTitle, submitTitle ] = useState({ title: '' })
+    const [ newDescription, submitDescription ] = useState({ description:'' })
+    const [ newPoster, submitPoster] = useState({ poster: '' })
+    const [ newGenre_id, submitGenre_id] = useState({ genre_id: '' })
+
+    // const [ title, submitTitle ] = useState('')
+    // const [ description, submitDescription ] = useState('')
+    // const [ poster, submitPoster] = useState('')
+    // const [ genre_id, submitGenre_id] = useState('')
     const dispatch = useDispatch
 
-    const submitTitle = event => {
-        dispatch({ type: "SET_TITLE", payload: {title: title }})
+
+
+    const addNewMovie = event => {
+        event.preventDefault();
+        dispatch({ type: "POST_NEW_MOVIE_TITLE", payload: newTitle })
+        dispatch({ type: "POST_NEW_MOVIE_TITLE", payload: newDescription })
+        dispatch({ type: "POST_NEW_MOVIE_TITLE", payload: newPoster })
+        dispatch({ type: "POST_NEW_MOVIE_TITLE", payload: newGenre_id })
     }
 
-    const submitDescription = event => {
-        dispatch({ type: "SET_DESCRIPTION", payload: { description: description }})
+      const handleSubmitTitle = event => {
+        dispatch({...newTitle, title: event.target.value})
     }
 
-    const submitPoster = event => {
-        dispatch({ type: "SET_POSTER", payload: { poster: poster }})
+    const handleSubmitDescription = event => {
+        dispatch({...newDescription, description: event.target.value})
     }
 
-    const submitGenre_id = event => {
-        dispatch({ type: "SET_GENRE_ID", payload: { genre_id: genre_id }})
+    const handleSubmitPoster = event => {
+        dispatch({...newPoster, poster: event.target.value})
     }
+
+    const handleSubmitGenre_id = event => {
+        dispatch({...newGenre_id, genre_id: event.target.value})
+    }
+    
+
+
+    // const submitTitle = event => {
+    //     dispatch({ type: "SET_TITLE", payload: {title: title }})
+    // }
+
+    // const submitDescription = event => {
+    //     dispatch({ type: "SET_DESCRIPTION", payload: { description: description }})
+    // }
+
+    // const submitPoster = event => {
+    //     dispatch({ type: "SET_POSTER", payload: { poster: poster }})
+    // }
+
+    // const submitGenre_id = event => {
+    //     dispatch({ type: "SET_GENRE_ID", payload: { genre_id: genre_id }})
+    // }
     
     return(
         <div>
             <h2>Add a New Movie</h2>
-            <form onSubmit >
+            <form>
+
+            </form>
+            {/* <form onSubmit >
                 <input type='text' placeholder='title' value={title} onChange={ (event) => submitTitle( event.target.value )} />
                 <input type='text' placeholder='description' value={description} onChange={ (event) => submitDescription( event.target.value )} />
                 <input type='text' placeholder='url' value={poster} onChange={ (event) => submitPoster( event.target.value )} />
                 <input type='text' placeholder='genre' value={genre_id} onChange={ (event) => submitGenre_id( event.target.value )} />
-            </form>
+            </form> */}
         </div>
     )
 }
